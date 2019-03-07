021643a2e53a54a85587d140481970d18cb7052c
x7d2b <x7d2b@ugrad.cs.ubc.ca>
2019-03-07 15:32:36 -0800

    Initial Commit

---
+++ ConcentrationMarch07/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConcentrationMarch07
+//
+//  Created by Lizhu Li on 2019-03-07.
+//  Copyright © 2019 Eve Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
